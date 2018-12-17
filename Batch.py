7829b81f23934101851c218161fdbcc517100dc0
Siergiej Nazarenko <snazarenko@human-ark.com>
2018-12-17 15:40:25 +0100

    fixed Houdini tests

--- Batch.py
+++ Batch.py
@@ -60,7 +60,6 @@ class BatchDebug(BatchBase):
         name = 'debug_images.py'
         tags = '/hafarm/debug_images'
         super(BatchDebug, self).__init__(name, tags, *args, **kwargs)
-        self.parms['command_arg'] = ['']
         self.parms['start_frame'] = kwargs.get('start', 1)
         self.parms['end_frame'] = kwargs.get('end', 1)
         scene_file_path, _, frame_padding_length, ext = utils.padding(filename)
@@ -68,7 +67,8 @@ class BatchDebug(BatchBase):
         path = os.path.join(path, const.DEBUG_POSTFIX)
         self.parms['pre_render_script'] = 'mkdir -p %s' % path
         self.parms['scene_file'] << { 'scene_fullpath' : scene_file_path + const.TASK_ID_PADDED + ext }
-        self.parms['exe'] = '$HAFARM_HOME/scripts/debug_images.py --job %s --save_json -i ' % self.parms['job_name']
+        self.parms['exe'] = '$HAFARM_HOME/scripts/debug_images.py'
+        self.parms['command_arg'] = ['--job %s'%self.parms['job_name'], '--save_json -i'] 
         self.parms['frame_padding_length'] = int(frame_padding_length)
         self.parms['job_name'] << { 'render_driver_type': 'debug' }
 
@@ -102,8 +102,8 @@ class BatchReportsMerger(BatchBase):
         log_path = os.path.join(path, const.DEBUG_POSTFIX)
         self.parms['job_name'] << { 'render_driver_type': 'reports' }
         self.parms['scene_file'] << { 'scene_file_path': log_path, 'scene_file_basename': scene_file_path, 'scene_file_ext': 'json' }
-        self.parms['exe'] = '$HAFARM_HOME/scripts/generate_render_report.py %s %s %s --mad_threshold %s --save_html ' % (send_email,
-                                             ifd_path, resend_frames, mad_threshold)
+        self.parms['exe'] = '$HAFARM_HOME/scripts/generate_render_report.py'
+        self.parms['command_arg'] = [ send_email, ifd_path, resend_frames, "--mad_threshold %s" % (mad_threshold),"--save_html"]
 
 
 class BatchJoinTiles(BatchBase):
